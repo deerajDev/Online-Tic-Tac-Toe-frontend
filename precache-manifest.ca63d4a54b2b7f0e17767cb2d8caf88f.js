27c9434f0cda9a131e17e42184b68bae74abae46
deeraj <deerajn04@gmail.com>
2020-06-26 14:36:58 +0530

    Updates

---
+++ precache-manifest.ca63d4a54b2b7f0e17767cb2d8caf88f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "888f0a15ca5891c5bc4154f013217eb9",
+    "url": "/Online-Tic-Tac-Toe-frontend/index.html"
+  },
+  {
+    "revision": "2adc2c951fbb4877a228",
+    "url": "/Online-Tic-Tac-Toe-frontend/static/css/main.033d14fb.chunk.css"
+  },
+  {
+    "revision": "b0bc8738072db57c525a",
+    "url": "/Online-Tic-Tac-Toe-frontend/static/js/2.d37c5a1b.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/Online-Tic-Tac-Toe-frontend/static/js/2.d37c5a1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2adc2c951fbb4877a228",
+    "url": "/Online-Tic-Tac-Toe-frontend/static/js/main.6bb33339.chunk.js"
+  },
+  {
+    "revision": "55ccb5bd46dc5e5041ee",
+    "url": "/Online-Tic-Tac-Toe-frontend/static/js/runtime-main.cafd457f.js"
+  }
+]);
